c1e0dce6c94a99a771b9bda52e9e7c5cfd9d8db0
Michael Schlimmer <mikkowus@gmail.com>
2022-01-16 19:08:00 +0000

    added new files

---
+++ howManyMilesTillItPaysForItself.py
@@ -0,0 +1,31 @@
+####################
+#This math will help you see how many miles need to be driven to pay off a new part in
+#your car
+####################
+
+partCost = 100 #y
+higherMPG = 21 #b higher mpg mpg1
+lowerMPG = 19 #a lower mpg mpg2
+gasPrice = 2.5 #z
+
+#costToDrive1 = (8000/mpg1*gasPrice)
+#costToDrive2 = (8000/mpg2*gasPrice)
+
+#print("CostToDrive1: ", costToDrive1)
+#print("CostToDrive2: ", costToDrive2)
+##################################################################
+
+#partCostX = (8000/19*gasPrice) - (8000/21*gasPrice)
+
+#print("PartCostX: ", partCostX)
+##################################################################
+
+#(a*b*y)/(-a*z+b*z)
+partCost = float(input("Cost of upgrade: "))
+higherMPG = float(input("new higher average mpg: "))
+lowerMPG = float(input("old lower average mpg: "))
+gasPrice = float(input("enter gas price: "))
+
+milageNeededToPayForPart = (lowerMPG*higherMPG*partCost)/(-lowerMPG*gasPrice+higherMPG*gasPrice)
+
+print("milage needed to pay for part: ", milageNeededToPayForPart)
